28680619d7ee4d508d28045e10cec7487412bad9
Savecoders <pablopincay2003@gmail.com>
2022-07-17 23:39:32 -0500

    learning sql syntaxis

--- mysql/test.sql
+++ mysql/test.sql
@@ -1,6 +1,20 @@
+
+create database sql_test;
+
+Select sql_test;
+
+/* Create table*/
 CREATE TABLE Persons (
     PersonID int,
     UserName varchar(255),
     Email varchar(255),
 );
 
+/* Add columm */
+alter table Persons add edad int;
+
+/* Delete columm */
+alter table persons drop column edad;
+
+/* Modify column email and size for characters*/
+alter table persons modify email varchar(50);
