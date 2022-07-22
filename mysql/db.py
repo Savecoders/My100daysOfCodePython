00785be7d3d0dfdabc6a9ae46734c891cb3a1984
Savecoders <pablopincay2003@gmail.com>
2022-07-21 20:45:51 -0500

    update, delete, insert data from my db

--- mysql/db.py
+++ mysql/db.py
@@ -15,9 +15,38 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
-cursor.execute('select * from Persons')
+#? show elements from Persons
+# select the table from Persons
+cursor.execute('select * from Persons limit 1')
+
+# show the columns from the table
+# cursor.execute('show create table Persons')
+
+#? insert data from the Table
+
+# sql = 'insert into Persons (username,email) values (%s, %s)'
+# values = ('Savecoders','myaddress@email.com')
+
+#? update data
+
+# sql = 'update Persons set email = %s where email = %s'
+# values = ('testsql@gmail.com', 'Salvador75WF@gmail.com')
+# cursor.execute(sql, values)
+
+#? delete data from the Table
+
+# sql = 'delete from Persons where UserName = %s'
+# later the first element of tuple, add the ','
+# values = ('Savecoders',)
+
+# cursor.execute(sql, values)
 
 
+#* save changes to the database
+# mydb.commit()
+
+# fetchall return all the instances of sql
+# fetchone return the first instance of sql
 data = cursor.fetchall()
 
 print(data)
