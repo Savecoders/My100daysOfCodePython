497ec345035a6406d23eade0b6c93d93bafe72b2
Savecoders <pablopincay2003@gmail.com>
2022-07-27 18:48:03 -0500

    fix errors of flask-init-db

--- flask/todoer/todo/schema.py
+++ flask/todoer/todo/schema.py
@@ -1,19 +1,19 @@
 
 instructions = [
-    'SET FOREIGN_KEY_CHECK=0;',
+    'SET FOREIGN_KEY_CHECKS=0;',
     'DROP TABLE IF EXISTS todo;',
     'DROP TABLE IF EXISTS user;',
-    'SET FOREIGN_KEY_CHECK=1;',
+    'SET FOREIGN_KEY_CHECKS=1;',
     """
         CREATE TABLE user (
-          id INT PRIMARY KEY AUTOINCREMENT,
-          name VARCHAR(55) NOT NULL UNIQUE,
-          password VARCHAR(255) NOT NULL
+          id INT PRIMARY KEY AUTO_INCREMENT,
+          username VARCHAR(55) UNIQUE NOT NULL,
+          password VARCHAR(100) NOT NULL
         )
     """,
     """
         CREATE TABLE todo (
-          id INT PRIMARY KEY AUTOINCREMENT,
+          id INT PRIMARY KEY AUTO_INCREMENT,
           created_by INT NOT NULL,
           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
           description TEXT NOT NULL,
