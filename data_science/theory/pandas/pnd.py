9386ca70f4521d99c371495414b7dc9dba4e7b06
Savecoders <pablopincay2003@gmail.com>
2022-08-16 22:40:57 -0500

    practice the theory of pandas

---
+++ data_science/theory/pandas/pnd.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+A = pd.Series([2, 3, 4, 5], index=['a', 'b', 'c', 'd'])
+
+type(A.values)
+# the type is numpy.ndarray
+
+type(A)
+# the type is:
+# pandas.core.series.Series
+
+grades_dict = {'A': 1, 'B': 2, 'C': 3}
+marks_dict = {'A': 32, 'B': 12, 'C': 22}
+
+grads = pd.Series(grades_dict)
+marks = pd.Series(marks_dict)
+
+print(grads.values)
+print(marks.values)
+
+# the atribute values return the
+# values keys of the dictionary
+
+myDataFrame = pd.DataFrame({'Marks': marks, 'Grads': grads}) 
+
+print(myDataFrame)
+# print the table 
+
+print(myDataFrame.T)
+# invert the column
+
+myDataFrame['ScaledMarks'] = 100*(myDataFrame['Marks'] / 90)
+
+print(myDataFrame)
+# add the column ScaledMarks
+
+
+ 
